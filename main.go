3f4d61ac0d6d04ba610b26aac2154d609f5ccb8f
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-11-20 15:16:20 -0500

    fixed typo and comment section

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "github.com/alinz/gonote/note"
 
 func main() {
-	l := lexer.NewLexerWithFilename("test1", "examples/array-of-constant.note")
+	l := note.NewLexerWithFilename("test1", "examples/array-of-constant.note")
 	l.Process()
 }
--- note/note-lex.go
+++ note/note-lex.go
@@ -1,6 +1,7 @@
 // Copyright 2014, Ali Najafizadeh. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // Author: Ali Najafizadeh
+
 package note
 
 import "strings"
